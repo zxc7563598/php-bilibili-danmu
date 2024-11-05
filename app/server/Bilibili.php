6227939a767736928281e1362111f1d076747157
苏青安 <992182040@qq.com>
2024-11-05 15:09:25 +0800

    调整websocket进程仅在存在cookie跟房间配置时连接，增加失败重连失败次数上限

--- app/server/Bilibili.php
+++ app/server/Bilibili.php
@@ -3,72 +3,98 @@
 namespace app\server;
 
 use Carbon\Carbon;
+use Exception;
 use Workerman\Worker;
 use Hejunjie\Bililive;
 use Workerman\Timer;
 use Workerman\Connection\AsyncTcpConnection;
 use Workerman\Protocols\Ws;
 use Hejunjie\Tools;
+use Random\RandomException;
 
 class Bilibili
 {
     private int $reconnectInterval = 5; // 重连间隔时间（秒）
-    private string $cookie;
-    private int $roomId;
+    private int $maxReconnectAttempts = 5; // 最大重连次数
+    private int $reconnectAttempts = 0; // 当前重连次数
+    private string|null $cookie; // 用户cookie
+    private int|null $roomId; // 直播间房间号
+    private ?int $heartbeatTimer = null; // 心跳
 
-    public function __construct()
+    public function onWorkerStart()
     {
-        // 设置cookie和房间ID（可替换成配置项或参数传入）
-        $this->cookie = 'SESSDATA=9b1247f4%2C1745588739%2C353a6%2Aa2CjDL5uIImsyB8VCUz2WBVbXhge8btcLj4q69VJogabjBJ5yWEhYfjU5qi2qN-gx38ZMSVlgxbEE0cFBQclhEZ3hlZ0M5TjBoUFYzai1MSXZYRG5CNm9hU0dRWTJzS2liSzl6RXFOcHJjenhRaWcwRkxJZllRMVVLbWJwSjhUeVYtc1BURnZTRll3IIEC;Path=/;Domain=bilibili.com;Expires=Fri, 25 Apr 2025 13:45:39 GMT;bili_jct=86205ff9b1081aa5e7395b0e711637b7;DedeUserID=4325051;DedeUserID__ckMd5=07816212060cbb4a;sid=8jzkqehf;buvid3=6D2B9B6A-8874-F002-90ED-55B9AFAF922339941infoc;buvid4=29F96FC2-498F-CAF4-97C8-1F4472E8C81F39941-024102713-+h273seQjZCLewAJjT6dK/Bt562wtyjJV4Sboe9zi5WMxJn/SmU+l3DQFv+tm7ma;b_nut=1730036739;refresh_token=414bc7c1e77a60fa436e9e32eaf596a2';
-        $this->roomId = 22384516;
-    }
-
-    public function onWorkerStart(Worker $worker)
-    {
-        // 创建 Unix 套接字监听
-        $socketFile = runtime_path() . '/bilibili.sock'; // 套接字文件路径，确保有权限访问
-        if (file_exists($socketFile)) {
-            unlink($socketFile); // 如果文件已存在，先删除以避免冲突
-        }
-        $unixWorker = new Worker("unix://$socketFile");
-        $unixWorker->onMessage = function ($connection, $data) {
-            if ($data === 'reload') {
-                // 执行重载逻辑
-                echo '重新启动啦';
-                // 添加具体的重载逻辑
-            }
-            $connection->send("Bilibili process handled: $data");
-        };
-
-        // 需要启动这个 Worker
-        $unixWorker->listen();
+        $this->startUnixWorker();
         $this->connectToWebSocket();
     }
 
     /**
+     * 启动 Unix Worker
+     * 
+     * @return void 
+     * @throws Exception 
+     */
+    private function startUnixWorker()
+    {
+        $socketFile = runtime_path() . '/bilibili.sock';
+        if (file_exists($socketFile)) {
+            unlink($socketFile);
+        }
+        $unixWorker = new Worker("unix://$socketFile");
+        $unixWorker->onMessage = function ($connection, $data) {
+            if ($data === 'reload') {
+                // 启动 websocket
+                $this->connectToWebSocket();
+            }
+            $connection->send("已处理Bilibili流程: $data");
+        };
+        $unixWorker->listen();
+    }
+
+    /**
+     * 读取文件信息
+     * 
+     * @param string $path 文件路径
+     * 
+     * @return null|string 
+     */
+    private function readFileContent(string $path): ?string
+    {
+        return (file_exists($path) && is_readable($path)) ? Tools\FileUtils::readFile($path) : null;
+    }
+
+    /**
      * 连接到 WebSocket 服务器
+     * 
+     * @return void 
+     * @throws Exception 
+     * @throws RandomException 
      */
     private function connectToWebSocket()
     {
-        // 获取真实房间号和WebSocket连接信息
-        $realRoomId = Bililive\Live::getRealRoomId($this->roomId, $this->cookie);
-        $wsData = Bililive\Live::getInitialWebSocketUrl($realRoomId, $this->cookie);
-
-        $wsUrl = 'ws://' . $wsData['host'] . ':' . $wsData['wss_port'] . '/sub';
-        $token = $wsData['token'];
-
-        // 创建 WebSocket 连接
-        $con = new AsyncTcpConnection($wsUrl);
-        $this->setupConnection($con, $realRoomId, $token);
-        $con->connect();
+        $this->cookie = $this->readFileContent(runtime_path() . '/tmp/cookie');
+        $this->roomId = $this->readFileContent(runtime_path() . '/tmp/connect');
+        if ($this->cookie && $this->roomId) {
+            // 获取真实房间号和WebSocket连接信息
+            $realRoomId = Bililive\Live::getRealRoomId($this->roomId, $this->cookie);
+            $wsData = Bililive\Live::getInitialWebSocketUrl($realRoomId, $this->cookie);
+            $wsUrl = 'ws://' . $wsData['host'] . ':' . $wsData['wss_port'] . '/sub';
+            $token = $wsData['token'];
+            // 创建 WebSocket 连接
+            $con = new AsyncTcpConnection($wsUrl);
+            $this->setupConnection($con, $realRoomId, $token);
+            $con->connect();
+        }
     }
 
     /**
      * 设置 WebSocket 连接的参数和回调
      * 
-     * @param AsyncTcpConnection $con
-     * @param int $roomId
-     * @param string $token
+     * @param AsyncTcpConnection $con 连接信息
+     * @param int $roomId 用户cookie
+     * @param string $token 直播间认证密钥
+     * 
+     * @return void 
+     * @throws RandomException 
      */
     private function setupConnection(AsyncTcpConnection $con, int $roomId, string $token)
     {
@@ -82,6 +108,7 @@ class Bilibili
         // 设置连接成功回调
         $con->onConnect = function (AsyncTcpConnection $con) use ($roomId, $token) {
             $this->onConnected($con, $roomId, $token);
+            $this->reconnectAttempts = 0; // 连接成功后重置重连计数器
         };
 
         // 设置消息接收回调
@@ -91,13 +118,15 @@ class Bilibili
 
         // 设置连接关闭回调
         $con->onClose = function () {
-            echo "Connection closed, attempting to reconnect...\n";
+            echo "连接已关闭，正在尝试重新连接...\n";
+            $this->clearTimers();
             $this->scheduleReconnect();
         };
 
         // 设置连接错误回调
         $con->onError = function ($connection, $code, $msg) {
-            echo "Error: $msg (code: $code), attempting to reconnect...\n";
+            echo "Error: $msg (code: $code), 尝试重新连接\n";
+            $this->clearTimers();
             $this->scheduleReconnect();
         };
     }
@@ -105,7 +134,7 @@ class Bilibili
     /**
      * 构建 WebSocket 请求的自定义 HTTP 头
      * 
-     * @return array
+     * @return array 
      */
     private function buildHeaders(): array
     {
@@ -127,36 +156,35 @@ class Bilibili
 
     /**
      * WebSocket连接成功时的处理
-     *
-     * @param AsyncTcpConnection $con
-     * @param int $roomId
-     * @param string $token
+     * 
+     * @param AsyncTcpConnection $con 连接信息
+     * @param int $roomId 用户cookie
+     * @param string $token 直播间认证密钥
+     * 
+     * @return void 
      */
     private function onConnected(AsyncTcpConnection $con, int $roomId, string $token)
     {
         echo "已连接到WebSocket,房间号:" . $roomId . "\n";
         // 发送认证包
-        // $con->send(Bililive\WebSocket::buildAuthPayload($roomId, $token, $this->cookie));
-
-        // 设置 websocket 心跳包发送定时器，每30秒发送一次
-        Timer::add(30, function () use ($con) {
+        $con->send(Bililive\WebSocket::buildAuthPayload($roomId, $token, $this->cookie));
+        $this->heartbeatTimer = Timer::add(30, function () use ($con, $roomId) {
             if ($con->getStatus() === AsyncTcpConnection::STATUS_ESTABLISHED) {
                 $con->send(Bililive\WebSocket::buildHeartbeatPayload());
-            }
-        });
-
-        // 设置 http 心跳包发送定时器，每60秒发送一次
-        Timer::add(60, function () use ($con, $roomId) {
-            if ($con->getStatus() === AsyncTcpConnection::STATUS_ESTABLISHED) {
-                $con->send(Bililive\Live::reportLiveHeartbeat($roomId, $this->cookie));
+                // 每隔两次（60秒）发送一次HTTP心跳包
+                if (Carbon::now()->second % 60 === 0) {
+                    $con->send(Bililive\Live::reportLiveHeartbeat($roomId, $this->cookie));
+                }
             }
         });
     }
 
     /**
      * 接收 WebSocket 消息时的处理
-     *
-     * @param mixed $data
+     * @param mixed $data 消息信息
+     * 
+     * @return void 
+     * @throws Exception 
      */
     private function onMessageReceived($data)
     {
@@ -192,18 +220,38 @@ class Bilibili
     }
 
     /**
+     * 清空定时器
+     * 
+     * @return void 
+     */
+    public function clearTimers()
+    {
+        if ($this->heartbeatTimer !== null) {
+            Timer::del($this->heartbeatTimer);
+            $this->heartbeatTimer = null;
+        }
+    }
+
+    /**
      * 设置重连的定时任务
+     * 
+     * @return void 
      */
     private function scheduleReconnect()
     {
+        $this->reconnectAttempts++; // 增加重连次数计数器
+        // 检查是否超过最大重连次数
+        if ($this->reconnectAttempts >= $this->maxReconnectAttempts) {
+            echo "已达到最大重连次数，不再尝试连接。\n";
+            Tools\FileUtils::fileDelete(runtime_path() . '/tmp/cookie');
+            Tools\FileUtils::fileDelete(runtime_path() . '/tmp/connect');
+            $this->cookie = null;
+            $this->roomId = null;
+            return;
+        }
+        // 设置重连定时器
         Timer::add($this->reconnectInterval, function () {
             $this->connectToWebSocket();
-            // $url = 'http://127.0.0.1:' . getenv('LISTEN') . '/reload-bilibili';
-            // $timestamp = Carbon::now()->timezone(config('app')['default_timezone'])->timestamp;
-            // Tools\HttpClient::sendPostRequest($url, [], [
-            //     'api_key' => md5(getenv('SECURE_API_KEY') . $timestamp),
-            //     'timestamp' => $timestamp
-            // ]);
         }, [], false);
     }
 }
