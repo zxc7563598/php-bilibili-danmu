53142285fdbc443827d16cbb2fd7723dcd092582
zxc7563598 <junjie.he.925@gmail.com>
2024-12-07 12:00:04 +0000

    权限调整

--- windows.php
+++ windows.php
@@ -5,7 +5,6 @@
 require_once __DIR__ . '/vendor/autoload.php';
 
 use Dotenv\Dotenv;
-use process\Monitor;
 use support\App;
 use Workerman\Worker;
 
@@ -28,12 +27,21 @@ if (isset($errorReporting)) {
 }
 
 $runtimeProcessPath = runtime_path() . DIRECTORY_SEPARATOR . '/windows';
-if (!is_dir($runtimeProcessPath)) {
-    mkdir($runtimeProcessPath);
-}
-$processFiles = [
-    __DIR__ . DIRECTORY_SEPARATOR . 'start.php'
+$paths = [
+    $runtimeProcessPath,
+    runtime_path('logs'),
+    runtime_path('views')
 ];
+foreach ($paths as $path) {
+    if (!is_dir($path)) {
+        mkdir($path, 0777, true);
+    }
+}
+
+$processFiles = [];
+if (config('server.listen')) {
+    $processFiles[] = __DIR__ . DIRECTORY_SEPARATOR . 'start.php';
+}
 foreach (config('process', []) as $processName => $config) {
     $processFiles[] = write_process_file($runtimeProcessPath, $processName, '');
 }
@@ -72,6 +80,14 @@ if (is_callable('opcache_reset')) {
     opcache_reset();
 }
 
+if (!\$appConfigFile = config_path('app.php')) {
+    throw new RuntimeException('Config file not found: app.php');
+}
+\$appConfig = require \$appConfigFile;
+if (\$timezone = \$appConfig['default_timezone'] ?? '') {
+    date_default_timezone_set(\$timezone);
+}
+
 App::loadAllConfig(['route']);
 
 worker_start('$processParam', $configParam);
@@ -90,7 +106,8 @@ EOF;
 }
 
 if ($monitorConfig = config('process.monitor.constructor')) {
-    $monitor = new Monitor(...array_values($monitorConfig));
+    $monitorHandler = config('process.monitor.handler');
+    $monitor = new $monitorHandler(...array_values($monitorConfig));
 }
 
 function popen_processes($processFiles)
