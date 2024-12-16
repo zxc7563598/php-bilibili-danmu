57b4b8cb2de937a1efb559921f08e6a0082082fd
zxc7563598 <junjie.he.925@gmail.com>
2024-12-16 14:44:15 +0800

    调整接口

---
+++ database/migrations/20241216133757_modify_goods_add_sale.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+use Phinx\Db\Adapter\MysqlAdapter;
+use Phinx\Migration\AbstractMigration;
+
+final class ModifyGoodsAddTips extends AbstractMigration
+{
+    /**
+     * Migrate Up.
+     */
+    public function up()
+    {
+        $table = $this->table('bl_goods');
+        $table->addColumn('sale_increase', 'integer', ['comment' => '每次销售递增', 'null' => false, 'limit' => MysqlAdapter::INT_MEDIUM, 'default' => 1, 'after' => 'sort'])
+            ->addColumn('sale_num', 'integer', ['comment' => '销售数量', 'null' => false, 'limit' => MysqlAdapter::INT_MEDIUM, 'default' => 0, 'after' => 'sort'])
+            ->save();
+    }
+
+    /**
+     * Migrate Down.
+     */
+    public function down()
+    {
+        $table = $this->table('bl_goods');
+        $table->removeColumn('sale_increase')
+            ->removeColumn('sale_num')
+            ->save();
+    }
+}
