f0e16967d1806be2f2d328e2c18935238e7de36a
zxc7563598 <junjie.he.925@gmail.com>
2024-12-20 16:14:49 +0800

    部分商城内容上传

---
+++ app/controller/shop/ManagementController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace app\controller\shop;
+
+use app\controller\GeneralMethod;
+use app\core\LoginPublicMethods;
+use app\model\UserVips;
+use support\Request;
+use support\Redis;
+use Webman\Http\Response;
+use resource\enums\UserVipsEnums;
+
+class ManagementController extends GeneralMethod
+{
+    public function pageDashboard(Request $request)
+    {
+        return view('shop/dashboard', [
+            'secretKey' => getenv('SECURE_API_KEY')
+        ]);
+    }
+    public function pageSystemConfiguration(Request $request)
+    {
+        return view('shop/system-configuration', [
+            'secretKey' => getenv('SECURE_API_KEY')
+        ]);
+    }
+    public function pageMallConfiguration(Request $request)
+    {
+        return view('shop/mall-configuration', [
+            'secretKey' => getenv('SECURE_API_KEY')
+        ]);
+    }
+    public function pageUserManagement(Request $request)
+    {
+        return view('shop/user-management', [
+            'secretKey' => getenv('SECURE_API_KEY')
+        ]);
+    }
+    public function pageProductManagement(Request $request)
+    {
+        return view('shop/product-management', [
+            'secretKey' => getenv('SECURE_API_KEY')
+        ]);
+    }
+    public function pageShippingManagement(Request $request)
+    {
+        return view('shop/shipping-management', [
+            'secretKey' => getenv('SECURE_API_KEY')
+        ]);
+    }
+    public function pageComplaintManagement(Request $request)
+    {
+        return view('shop/complaint-management', [
+            'secretKey' => getenv('SECURE_API_KEY')
+        ]);
+    }
+    public function pageFeedback(Request $request)
+    {
+        return view('shop/feedback', [
+            'secretKey' => getenv('SECURE_API_KEY')
+        ]);
+    }
+}
