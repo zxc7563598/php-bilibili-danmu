94c7d0096b504964d9299ac2e6e723313c6881e4
zxc7563598 <junjie.he.925@gmail.com>
2025-01-09 13:39:11 +0800

    增加第一版邮件发送

--- database/migrations/20250109105820_add_shop_config_email_data.php
+++ database/migrations/20250109105820_add_shop_config_email_data.php
@@ -13,7 +13,7 @@ final class AddShopConfigEmailData extends AbstractMigration
     {
         $tableAdd = $this->table('bl_shop_config');
         $tableAdd->insert([
-            ['config_id' => 29, 'title' => 'enable-aggregate-mail', 'description' => '下播邮件', 'content' => '1', 'created_at' => time(), 'updated_at' => time()],
+            ['config_id' => 29, 'title' => 'enable-aggregate-mail', 'description' => '是否开启下播邮件', 'content' => '1', 'created_at' => time(), 'updated_at' => time()],
             ['config_id' => 30, 'title' => 'email-address', 'description' => '邮箱', 'content' => '', 'created_at' => time(), 'updated_at' => time()],
             ['config_id' => 31, 'title' => 'address-as', 'description' => '邮箱用户名', 'content' => '', 'created_at' => time(), 'updated_at' => time()]
         ]);
