306f504eb49df6fb029df49529f4bec102218e33
苏青安 <992182040@qq.com>
2025-03-12 12:27:25 +0800

    调整自动填充数据

--- database/migrations/20250312115743_add_check_in_data.php
+++ database/migrations/20250312115743_add_check_in_data.php
@@ -17,8 +17,8 @@ final class AddCheckInData extends AbstractMigration
             ['config_id' => 34, 'title' => 'check-in', 'description' => '是否开启签到', 'content' => '1', 'created_at' => time(), 'updated_at' => time()],
             ['config_id' => 35, 'title' => 'check-in-keywords', 'description' => '签到词', 'content' => '#签到', 'created_at' => time(), 'updated_at' => time()],
             ['config_id' => 36, 'title' => 'check-in-select', 'description' => '签到查询词', 'content' => '#查询', 'created_at' => time(), 'updated_at' => time()],
-            ['config_id' => 37, 'title' => 'check-in-success', 'description' => '签到成功回复', 'content' => "恭喜@name@签到成功\r\n@name@您已经签到@day@天啦\r\n签到成功", 'created_at' => time(), 'updated_at' => time()],
-            ['config_id' => 38, 'title' => 'check-in-reply', 'description' => '签到查询回复', 'content' => "@name@已经连续签到@day@天啦\r\n已经签到@day@天了", 'created_at' => time(), 'updated_at' => time()]
+            ['config_id' => 37, 'title' => 'check-in-success', 'description' => '签到成功回复', 'content' => "恭喜@name@签到成功\r\n\r\n@name@您已经签到@day@天啦\r\n\r\n签到成功", 'created_at' => time(), 'updated_at' => time()],
+            ['config_id' => 38, 'title' => 'check-in-reply', 'description' => '签到查询回复', 'content' => "@name@已经连续签到@day@天啦\r\n\r\n已经签到@day@天了", 'created_at' => time(), 'updated_at' => time()]
         ]);
         $tableAdd->saveData();
     }
