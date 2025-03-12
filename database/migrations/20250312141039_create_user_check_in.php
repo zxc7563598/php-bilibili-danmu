d0f4fdabe502c2a08ce5c9ce6289ba3d63b68d8c
苏青安 <992182040@qq.com>
2025-03-12 14:33:31 +0800

    用户表增加累计签到天数与连续签到天数&新增用户签到表

---
+++ database/migrations/20250312141039_create_user_check_in.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+use Phinx\Db\Adapter\MysqlAdapter;
+use Phinx\Migration\AbstractMigration;
+
+final class CreateUserCheckIn extends AbstractMigration
+{
+    /**
+     * Migrate Up.
+     */
+    public function up()
+    {
+        $table = $this->table('bl_user_check_in', ['id' => 'log_id', 'comment' => '用户模块 - 用户签到表']);
+        $table->addColumn('uid', 'string', ['comment' => 'uid', 'null' => false])
+            ->addColumn('name', 'string', ['comment' => '名称', 'null' => false])
+            ->addColumn('ruid', 'string', ['comment' => '用户携带的牌子归属主播的uid', 'null' => false])
+            ->addColumn('guard_level', 'string', ['comment' => '大航海类型，0=普通用户，1=总督，2=提督，3=舰长', 'null' => false])
+            ->addColumn('created_at', 'integer', ['comment' => '创建时间', 'null' => false, 'limit' => MysqlAdapter::INT_BIG])
+            ->addColumn('updated_at', 'integer', ['comment' => '更新时间', 'null' => true, 'limit' => MysqlAdapter::INT_BIG])
+            ->addColumn('deleted_at', 'integer', ['comment' => '逻辑删除', 'null' => true, 'limit' => MysqlAdapter::INT_BIG])
+            ->create();
+    }
+
+    /**
+     * Migrate Down.
+     */
+    public function down()
+    {
+        $this->table('bl_user_check_in')->drop()->save();
+    }
+}
