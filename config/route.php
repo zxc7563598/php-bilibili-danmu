4e2ab499d84c9a1a9fa63cf737062b90b7bdff8e
苏青安 <992182040@qq.com>
2025-05-20 17:53:27 +0800

    最基本的用户每日分析完成

--- config/route.php
+++ config/route.php
@@ -64,6 +64,9 @@ Route::group('/admin-api', function () { // 后台管理系统接口
     Route::post('/others/gift-info/get-statistic-data', [admin\others\GiftInfoController::class, 'getStatisticData'])->name('[其他-礼物信息-获取统计数据]');
     // 其他 - 弹幕信息
     Route::post('/others/danmaku-info/get-data', [admin\others\DanmakuInfoController::class, 'getData'])->name('[其他-弹幕信息-获取列表数据]');
+    // 其他 - 用户分析
+    Route::post('/others/user-analysis/get-data', [admin\others\UserAnalysisController::class, 'getData'])->name('[其他-用户分析-获取列表数据]');
+    Route::post('/others/user-analysis/get-daily-active', [admin\others\UserAnalysisController::class, 'getDailyActive'])->name('[其他-用户分析-获取每月分析数据]');
     // 其他 - 投诉管理
     Route::any('/others/complaint-management/get-data', [admin\others\ComplaintManagementController::class, 'getData'])->name('[其他-投诉管理-获取投诉数据列表]');
     Route::any('/others/complaint-management/get-data-details', [admin\others\ComplaintManagementController::class, 'getDataDetails'])->name('[其他-投诉管理-获取投诉详情]');
