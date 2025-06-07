1dbb0015503cc91753d28dc428ec7c0fe22421a9
zxc7563598 <junjie.he.925@gmail.com>
2025-06-07 17:50:07 +0800

    feat(migration): 系统变更积分记录表增加积分类型，用以支撑多种积分的变更

---
+++ resource/enums/SystemChangePointRecordsEnums/PointType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace resource\enums\SystemChangePointRecordsEnums;
+
+/**
+ * 变更积分类型
+ */
+enum PointType: int
+{
+    case Point = 0;
+    case RewardPoint = 1;
+
+    //定义一个转换函数，用来显示
+    public function label(): string
+    {
+        return match ($this) {
+            static::Point => '积分',
+            static::RewardPoint => '奖励积分'
+        };
+    }
+
+    // 获取全部的枚举
+    public static function all(): array
+    {
+        $cases = self::cases();
+        $enums = [];
+        foreach ($cases as $_cases) {
+            $enums[] = [
+                'key' => $_cases->value,
+                'value' => $_cases->label()
+            ];
+        }
+        return $enums;
+    }
+}
