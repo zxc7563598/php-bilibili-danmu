c311e875fb9aec22449aad4bd27173660c6b7d46
苏青安 <992182040@qq.com>
2025-06-08 17:10:14 +0800

    docs(migration): 增加后台的版本更新说明

---
+++ database/migrations/20250608170527_add_update_logs_version4.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+use Phinx\Migration\AbstractMigration;
+use support\Redis;
+
+final class AddUpdateLogsVersion4 extends AbstractMigration
+{
+    /**
+     * Migrate Up.
+     */
+    public function up()
+    {
+        $tableAdd = $this->table('bl_admin_update_logs');
+        $tableAdd->insert([
+            [
+                "id" => 4,
+                "title" => "签到增加硬币概念",
+                "description" => "版本v1.2.0",
+                "version" => "v1.2.0",
+                "content" => "新增硬币概念\n签到奖励不再计入积分\n而是单独作为硬币计算\n\n商品可选择使用积分\n或硬币进行出售\n\n此项更新为必需更新项\n未更新将导致商品设置功能异常\n\n请尽快更新以确保正常使用\n\n---------------------------------------------\n\n更新方法：拉取最新代码，重新打包覆盖服务器文件。\n\n如果觉得功能还不错，欢迎顺手点个 Star，支持是继续优化的动力。\n如有问题欢迎联系我，我会尽力协助。\n\n祝你使用愉快！",
+                "meta" => time(),
+                "created_at" => time(),
+                "updated_at" => time(),
+                "deleted_at" => null
+            ]
+        ]);
+        $tableAdd->saveData();
+    }
+
+    /**
+     * Migrate Down.
+     */
+    public function down()
+    {
+        $this->execute("DELETE FROM `bl_admin_update_logs` WHERE `id` in (4)");
+    }
+}
