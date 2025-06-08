4cd99cd97168c815e860114fa25120b1f20004aa
zxc7563598 <junjie.he.925@gmail.com>
2025-06-08 11:33:00 +0800

    feat(migration): 商品兑换记录表增加 amount_type 来区分货币类型

---
+++ database/migrations/20250608112548_modify_redemption_records_add_amount_type.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+use Phinx\Db\Adapter\MysqlAdapter;
+use Phinx\Migration\AbstractMigration;
+
+final class ModifyRedemptionRecordsAddAmountType extends AbstractMigration
+{
+    public function up(): void
+    {
+        $table = $this->table('bl_redemption_records');
+        $table->addColumn('amount_type', 'integer', ['comment' => '商品价格类型', 'null' => false, 'default' => 0, 'after' => 'amount', 'limit' => MysqlAdapter::INT_TINY])
+            ->save();
+    }
+
+    public function down(): void
+    {
+        $table = $this->table('bl_redemption_records');
+        $table->removeColumn('amount_type')
+            ->save();
+    }
+}
