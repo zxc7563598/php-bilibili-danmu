2c9a00c912acb0778c56279e919b2f170c8467da
zxc7563598 <junjie.he.925@gmail.com>
2026-06-24 10:07:50 +0800

    fix(migration): 更新迁移类名为AddUpdateLogsVersion17

--- database/migrations/20260624100340_add_update_logs_version_17.php
+++ database/migrations/20260624100340_add_update_logs_version_17.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 use Phinx\Migration\AbstractMigration;
 
-final class AddUpdateLogsVersion16 extends AbstractMigration
+final class AddUpdateLogsVersion17 extends AbstractMigration
 {
     /**
      * Change Method.
